4fcdfdfa7350a22e34ca32d6a4e22115a30e82bf
wendell91097 <wendell91097@gmail.com>
2023-03-03 20:13:35 -0500

    second commit

--- roi.py
+++ roi.py
@@ -5,170 +5,46 @@ class ReturnCalculcator():
     To use this, establish an instance of the ReturnCalculator class and execute .run()
     
     """
-    def __init__(self, rent = 0, income = 0, expenses = 0, investment = 0):
-        self.rent = rent
-        self.income = income
-        self.expenses = expenses
-        self.investment = investment
+    def __init__(self):
+        self.rent = 0  
+        self.income = 0
+        self.expenses = 0
+        self.investment = 0
 
     def cashOnCashReturn(self):
-        print('\nFinally, to find your initial expenses. ')
-        while True:
-            temporaryInvestment = input('\nEnter the down payment on the property. ')
-            try:
-                self.investment += int(temporaryInvestment)
-                print(f"\nOkay, your down payment was about ${self.investment}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInvestment = input('\nEnter the closing costs. ')
-            try:
-                self.investment += int(temporaryInvestment)
-                print(f"\nOkay, the closing cost was about ${temporaryInvestment}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInvestment = input('\nEnter the rehab costs. ')
-            try:
-                self.investment += int(temporaryInvestment)
-                print(f"\nOkay, rehab costs was about ${temporaryInvestment}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInvestment = input('\nEnter any other miscellaneous costs. ')
-            try:
-                self.investment += int(temporaryInvestment)
-                print(f"\nOkay, the misc costs was about ${temporaryInvestment}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        print(f"\Your initial investment was ${self.investment}.")
-        print(f"\nYour monthly income is about ${self.income}.")
+        print('Finally, to find your initial expenses. ')
+        response1 = int(input('\nEnter the down payment on the property. '))
+        response2 = int(input('\nEnter the closing costs. '))
+        response3 = int(input('\nEnter the rehab costs. '))
+        response4 = int(input('\nEnter any other miscellaneous costs. '))
+        self.investment = response1 + response2 + response3 + response4
+        print(f"Your monthly income is about ${self.income}.")
         print(f"Your monthly expenses are ${self.expenses}.")
         returnOnInvestment = round((((self.income - self.expenses)*1200)/self.investment), 2)
         print(f"\nYour return on investment for this property would be {returnOnInvestment}% annually.")
         print("\nIs that good? That depends on your goals, your strategy, what you're trying to do \nand what other investments you're trying to have.")
     def expensesCalculator(self):
-        print("\nNext is to find your average monthly expenses on the property.")
-        while True:
-            temporaryExpenses = input('\nPlease enter your monthly tax expense. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your monthly taxes are about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryExpenses = input('\nPlease enter your monthly insurance expense. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your monthly insurance expense is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryExpenses = input('\nPlease enter your monthly utility expense. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your  monthly utility expense is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryExpenses = input('\nPlease enter the monthly HOA fee. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your HOA fee is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryExpenses = input('\nPlease enter the average monthly lawn/snow cost. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your lawn/snow cost is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryPercent = input('\nEnter the percent of rent income you save for vacancy cost (usually about 3% to 4%). Do not include a \'%\' sign. ')
-            try:
-                temporaryPercent = float(temporaryPercent)
-                temporaryExpenses = (self.rent * temporaryPercent)/100
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your vacancy savings is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a percent sign or you're simply a troublemaker.")
-        while True:
-            temporaryPercent = input('\nEnter the percent of rent income you save for the cost of repairs (around 5%). Do not include a \'%\' sign. ')
-            try:
-                temporaryPercent = float(temporaryPercent)
-                temporaryExpenses = (self.rent * temporaryPercent)/100
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your monthly repairs savings is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a percent sign or you're simply a troublemaker.")
-        while True:
-            temporaryPercent = input('\nEnter the percent of rent income you save for capital expenditures (around 5%). Do not include a \'%\' sign. ')
-            try:
-                temporaryPercent = float(temporaryPercent)
-                temporaryExpenses = (self.rent * temporaryPercent)/100
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your capital expenditures savings is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a percent sign or you're simply a troublemaker.")
-        while True:
-            temporaryExpenses = input('\nPlease enter the monthly mortgage. ')
-            try:
-                self.expenses += int(temporaryExpenses)
-                print(f"\nOkay, your mortgage is about ${int(temporaryExpenses)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        print(f"Your monthly expenses are ${self.expenses}.")
+        expense1 = int(input('\nPlease enter your monthly tax expense. '))
+        expense2 = int(input('\nPlease enter your monthly insurance expense. '))
+        expense3 = int(input('\nPlease enter your monthly utility expense. '))
+        expense4 = int(input('\nPlease enter the monthly HOA fee. '))
+        expense5 = int(input('\nPlease enter the average monthly lawn/snow cost. '))
+        expense6 = int(input('\nEnter the $ amount you save for vacancy cost. '))
+        expense7 = int(input('\nEnter the $ amount you save for the cost of repairs. '))
+        expense8 = int(input('\nEnter the $ amount you save for capital expenditures. '))
+        expense9 = int(input('\nPlease enter the monthly mortgage. '))
+        
+        self.expenses = expense1 + expense2 + expense3 + expense4 + expense5 + expense6 + expense7 + expense8 + expense9
+        print(f"YOur monthly expenses are: ${self.expenses}.")
     def incomeCalculator(self):
-        print("\nTo calculate your return on investment we need some information. Enter 0 if nothing applies.")
-        print("\nEnter numbers without any dollar or cent signs.")
-        print("\nFirst is to find your average monthly income on the property. ")
-        while True:
-            temporaryInput = input('\nEnter the monthly rental income on the property. ')
-            try:
-                self.income += int(temporaryInput)
-                self.rent = int(temporaryInput)
-                print(f"\nOkay, your rental income is about ${self.income}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInput = input('\nEnter the monthly laundry income on the property. ')
-            try:
-                self.income += int(temporaryInput)
-                print(f"\nOkay, your laundry income is about ${int(temporaryInput)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInput = input('\nEnter the monthly storage income on the property. ')
-            try:
-                self.income += int(temporaryInput)
-                print(f"\nOkay, your storage income is about ${int(temporaryInput)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
-        while True:
-            temporaryInput = input('\nEnter any other monthly income on the property. ')
-            try:
-                self.income += int(temporaryInput)
-                print(f"\nOkay, your misc. income is about ${int(temporaryInput)}")
-                break
-            except:
-                print("\nIt seems either you included a dollar/cent sign or you're simply a troublemaker.")
+        print("To calculate your return on investment we need some information. Enter 0 if nothing applies.")
+        print("Enter numbers without any dollar or cent signs.")
+        print("First is to find your average monthly income on the property. ")
+        income1 = int(input('\nEnter the monthly rental income on the property. '))
+        income2 = int(input('\nEnter the monthly laundry income on the property. '))
+        income3 = int(input('\nEnter the monthly storage income on the property. '))
+        income4 = int(input('\nEnter any other monthly income on the property. '))
+        self.income = income1 + income2 + income3 + income4
         print(f"\nYour monthly income is about ${self.income}.")
 
     def run(self):
@@ -179,5 +55,4 @@ class ReturnCalculcator():
 go = ReturnCalculcator()
 
 go.run()
-
 
